c2936610ca7feafd365aed3a6536b1cf51edcfe2
Roberto Moran <byrobertomoran@gmail.com>
2024-09-16 20:13:28 -0600

    Update ticket_opciones_crud.dart

--- lib/CRUD/ticket_opciones_crud.dart
+++ lib/CRUD/ticket_opciones_crud.dart
@@ -1,14 +1,14 @@
-// ticket_crud.dart
+
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:examen_progra_firebase/model/ticket_model.dart';
 
-// Agregar Ticket (Crear)
+
 Future<void> agregarTicket(Ticket ticket) async {
   await FirebaseFirestore.instance.collection('TicketAvion').add(ticket.toMap());
 }
 
-// Leer Tickets (Listar)
+
 Stream<List<Ticket>> leerTickets() {
   return FirebaseFirestore.instance
       .collection('TicketAvion')
@@ -18,7 +18,6 @@ Stream<List<Ticket>> leerTickets() {
           .toList());
 }
 
-// Actualizar Ticket
 Future<void> actualizarTicket(Ticket ticket) async {
   await FirebaseFirestore.instance
       .collection('TicketAvion')
@@ -26,7 +25,7 @@ Future<void> actualizarTicket(Ticket ticket) async {
       .update(ticket.toMap());
 }
 
-// Eliminar Ticket
+
 Future<void> eliminarTicket(String id) async {
   await FirebaseFirestore.instance.collection('TicketAvion').doc(id).delete();
 }

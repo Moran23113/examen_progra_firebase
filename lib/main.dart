0a830d6ce0141eb724718701d5d5d44bcab433af
Roberto Moran <byrobertomoran@gmail.com>
2024-09-16 20:43:31 -0600

    info mas detallada de los tickets

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,62 @@
+import 'package:examen_progra_firebase/pages/listatickets.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+import 'pages/ticketprovider.dart';
+import 'pages/ticketedit.dart';
+import 'pages/ticketdetail.dart';
+import 'pages/ticketdelete.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => TicketProvider(),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      routerDelegate: GoRouter(
+        routes: [
+          GoRoute(
+            path: '/',
+            builder: (context, state) => TicketListScreen(),
+            routes: [
+              GoRoute(
+                path: 'edit/:id',
+                builder: (context, state) {
+                  final id = state.params['id'];
+                  return TicketEditScreen(id: id!);
+                },
+              ),
+              GoRoute(
+                path: 'detail/:id',
+                builder: (context, state) {
+                  final id = state.params['id'];
+                  return TicketDetailScreen(id: id!);
+                },
+              ),
+              GoRoute(
+                path: 'delete/:id',
+                builder: (context, state) {
+                  final id = state.params['id'];
+                  return DeleteConfirmationScreen(id: id!);
+                },
+              ),
+            ],
+          ),
+        ],
+      ).routerDelegate,
+      routeInformationParser: GoRouter().routeInformationParser,
     );
   }
 }
---
+++ lib/pages/ticketdetail.dart
@@ -0,0 +1,55 @@
+// ticket_detail_screen.dart
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:examen_progra_firebase/pages/ticketprovider.dart';
+import 'package:go_router/go_router.dart';
+
+class TicketDetailScreen extends StatelessWidget {
+  final String id;
+
+  TicketDetailScreen({required this.id});
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<TicketProvider>(context);
+    final ticket = provider.tickets.firstWhere((t) => t.id == id);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Ticket Details')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Número de vuelo: ${ticket.numeroVuelo}'),
+            Text('Aerolínea: ${ticket.aerolinea}'),
+            Text('Pasajero: ${ticket.pasajero}'),
+            Text('Origen: ${ticket.origen}'),
+            Text('Destino: ${ticket.destino}'),
+            Text('Asiento: ${ticket.asiento}'),
+            Text('Clase: ${ticket.clase}'),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    GoRouter.of(context).go('/edit/${ticket.id}');
+                  },
+                  child: const Text('Editar'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    GoRouter.of(context).go('/delete/${ticket.id}');
+                  },
+                  child: const Text('Eliminar'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

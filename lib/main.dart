553a4c785813b2a0fb0527e19e0b0c51c99a3b80
Roberto Moran <byrobertomoran@gmail.com>
2024-09-16 20:47:44 -0600

    avance de go router y delete page

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
-import 'package:examen_progra_firebase/pages/listatickets.dart';
+// ignore_for_file: use_key_in_widget_constructors, no_leading_underscores_for_local_identifiers
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
-
-import 'pages/ticketprovider.dart';
-import 'pages/ticketedit.dart';
-import 'pages/ticketdetail.dart';
-import 'pages/ticketdelete.dart';
+import 'pages/ticketprovider.dart'; 
+import 'pages/ticketedit.dart'; 
+import 'pages/ticketdetail.dart'; 
+import 'pages/ticketdelete.dart'; 
+import 'pages/listatickets.dart'; 
 
 void main() {
   runApp(
@@ -24,39 +25,41 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final GoRouter _router = GoRouter(
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) => TicketListScreen(),
+          routes: [
+            GoRoute(
+              path: 'edit/:id',
+              builder: (context, state) {
+                final id = state.pathParameters['id'];
+                return TicketEditScreen(id: id!);
+              },
+            ),
+            GoRoute(
+              path: 'detail/:id',
+              builder: (context, state) {
+                final id = state.pathParameters['id'];
+                return TicketDetailScreen(id: id!);
+              },
+            ),
+            GoRoute(
+              path: 'delete/:id',
+              builder: (context, state) {
+                final id = state.pathParameters['id'];
+                return DeleteConfirmationScreen(id: id!);
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+
     return MaterialApp.router(
-      routerDelegate: GoRouter(
-        routes: [
-          GoRoute(
-            path: '/',
-            builder: (context, state) => TicketListScreen(),
-            routes: [
-              GoRoute(
-                path: 'edit/:id',
-                builder: (context, state) {
-                  final id = state.params['id'];
-                  return TicketEditScreen(id: id!);
-                },
-              ),
-              GoRoute(
-                path: 'detail/:id',
-                builder: (context, state) {
-                  final id = state.params['id'];
-                  return TicketDetailScreen(id: id!);
-                },
-              ),
-              GoRoute(
-                path: 'delete/:id',
-                builder: (context, state) {
-                  final id = state.params['id'];
-                  return DeleteConfirmationScreen(id: id!);
-                },
-              ),
-            ],
-          ),
-        ],
-      ).routerDelegate,
-      routeInformationParser: GoRouter().routeInformationParser,
+      routerDelegate: _router.routerDelegate,
+      routeInformationParser: _router.routeInformationParser,
     );
   }
 }
---
+++ lib/pages/ticketdelete.dart
@@ -0,0 +1,47 @@
+
+import 'package:examen_progra_firebase/pages/ticketprovider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class DeleteConfirmationScreen extends StatelessWidget {
+  final String id;
+
+  DeleteConfirmationScreen({required this.id});
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<TicketProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Confirmación de Eliminación')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const Text('¿Estás seguro de que deseas eliminar este ticket?'),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    await provider.deleteTicket(id);
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Eliminar'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancelar'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
